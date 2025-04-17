f37215adc43a93cdbe99fff34a0d094ffb272844
mosch <mohammed.scherg@outlook.de>
2025-04-17 17:40:51 +0200

    added prismjs as an option and fixed bug where inputs are deleted

--- H5P.SyntaxBlanks/src/syntax-blanks.js
+++ H5P.SyntaxBlanks/src/syntax-blanks.js
@@ -1,49 +1,65 @@
 import hljs from "highlight.js";
+import prism from "prismjs";
 import decodeHTMLEntities from './decodeSanitzied';
 
 H5P.SyntaxBlanks = (function ($, Blanks) {
-    /**
-     * @class
-     * @extends H5P.Blanks
-     */
-    function SyntaxBlanks(params, contentId, contentData) {
+  /**
+   * @class
+   * @extends H5P.Blanks
+   */
+  function SyntaxBlanks(params, contentId, contentData) {
 
 
-      Blanks.call(this, params, contentId, contentData);
+    Blanks.call(this, params, contentId, contentData);
+  }
+
+  // Inherit from H5P.Blanks
+  SyntaxBlanks.prototype = Object.create(Blanks.prototype);
+  SyntaxBlanks.prototype.constructor = SyntaxBlanks;
+
+  SyntaxBlanks.prototype.handleBlanks = function (question, handler) {
+
+    const self = this;
+
+    console.log(question);
+
+    if(self.params.behaviour.enableSyntaxHighlighting) {
+
+      question = self.highlightAll(
+        question, 
+        { language: self.params.behaviour.syntaxLanguage }
+      );
+    }  
+    
+    question = `<pre><code class="language-${self.params.behaviour.syntaxLanguage}">${question}</code></pre>`;
+
+    console.log(question);
+
+    let res = Blanks.prototype.handleBlanks.call(self, question, handler);
+
+    return res;
+  }
+
+
+  SyntaxBlanks.prototype.highlightAll = function (question, options) {
+    const self = this;
+
+    const rawCode = decodeHTMLEntities(question);
+    return self.highlightFactory(rawCode, options.language);
+  }
+
+  SyntaxBlanks.prototype.highlightFactory = function(code, language) {
+
+    if (this.params.behaviour.highlightingLibrary === 'prismjs') {
+
+      return Prism.highlight(code, Prism.languages[language], language);
+
+    } else {
+
+      // Fallback to default highlighting
+      return hljs.highlight(code, { language: language }).value;
     }
+  }
   
-    // Inherit from H5P.Blanks
-    SyntaxBlanks.prototype = Object.create(Blanks.prototype);
-    SyntaxBlanks.prototype.constructor = SyntaxBlanks;
-  
-
-    SyntaxBlanks.prototype.createQuestions = function () {
-
-        const self = this;
-
-        var $questions = Blanks.prototype.createQuestions.call(self);
-
-        if (self.params.behaviour.enableSyntaxHighlighting) {
-
-            self.highlightAll(
-                $questions, 
-                {language: self.params.behaviour.syntaxLanguage}
-            );
-        }
-
-        return $questions;
-    };
-
-
-    SyntaxBlanks.prototype.highlightAll = function ($questions, options) {
-      // Iterate through each question and apply syntax highlighting
-      $questions.each(function () {
-        const rawCode = decodeHTMLEntities($(this).html());
-        const highlightedCode = hljs.highlight(rawCode, { language: options.language }).value;
-        $(this).html(highlightedCode);
-      });
-    }
-  
-    return SyntaxBlanks;
-  })(H5P.jQuery, H5P.Blanks);
-  
+  return SyntaxBlanks;
+})(H5P.jQuery, H5P.Blanks);
