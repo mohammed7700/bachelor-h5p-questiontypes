2210772839fb1299994f0453c9e50989c7ebab6c
mosch <mohammed.scherg@outlook.de>
2025-04-20 02:51:46 +0200

    implemented solution for sanitizing solution string from html elements created by a syntax highlighter

--- H5P.SyntaxBlanks/src/syntax-blanks.js
+++ H5P.SyntaxBlanks/src/syntax-blanks.js
@@ -9,6 +9,7 @@ H5P.SyntaxBlanks = (function ($, Blanks) {
    */
   function SyntaxBlanks(params, contentId, contentData) {
 
+    const self = this;
 
     Blanks.call(this, params, contentId, contentData);
   }
@@ -17,27 +18,62 @@ H5P.SyntaxBlanks = (function ($, Blanks) {
   SyntaxBlanks.prototype = Object.create(Blanks.prototype);
   SyntaxBlanks.prototype.constructor = SyntaxBlanks;
 
+
+  /**
+   * Find blanks in a string and run a handler on those blanks
+   *
+   * @param {string} question
+   *   Question text containing blanks enclosed in asterisks.
+   * @param {function} handler
+   *   Replaces the blanks text with an input field.
+   * @returns {string}
+   *   The question with blanks replaced by the given handler.
+   */
   SyntaxBlanks.prototype.handleBlanks = function (question, handler) {
 
     const self = this;
 
     console.log(question);
 
-    if(self.params.behaviour.enableSyntaxHighlighting) {
+    if(self.params.highLightingBehaviour.enableSyntaxHighlighting) {
 
       question = self.highlightAll(
         question, 
-        { language: self.params.behaviour.syntaxLanguage }
+        { language: self.params.highLightingBehaviour.syntaxLanguage }
       );
-    }  
-    
-    question = `<pre><code class="language-${self.params.behaviour.syntaxLanguage}">${question}</code></pre>`;
+
+      question = `<pre><code class="language-${self.params.highLightingBehaviour.syntaxLanguage}">${question}</code></pre>`;
+    }
 
     console.log(question);
 
-    let res = Blanks.prototype.handleBlanks.call(self, question, handler);
+    const markingChar = self.params.clozerMarkingCharacter;
 
-    return res;
+    // Go through the text and run handler on all asterisk
+    var clozeEnd, clozeStart = question.indexOf(markingChar);
+    
+    while (clozeStart !== -1 && clozeEnd !== -1) {
+      clozeStart++;
+      clozeEnd = question.indexOf(markingChar, clozeStart);
+      if (clozeEnd === -1) {
+        continue; // No end
+      }
+      var clozeContent = question.substring(clozeStart, clozeEnd).replaceAll(/<\/?[a-z\-=\ "'\_]*\d?>/ig, '');
+      var replacer = '';
+      if (clozeContent.length) {
+        replacer = handler(self.parseSolution(clozeContent));
+        clozeEnd++;
+      }
+      else {
+        clozeStart += 1;
+      }
+      question = question.slice(0, clozeStart - 1) + replacer + question.slice(clozeEnd);
+      clozeEnd -= clozeEnd - clozeStart - replacer.length;
+
+      // Find the next cloze
+      clozeStart = question.indexOf(markingChar, clozeEnd);
+    }
+    return question;
   }
 
 
@@ -50,9 +86,11 @@ H5P.SyntaxBlanks = (function ($, Blanks) {
 
   SyntaxBlanks.prototype.highlightFactory = function(code, language) {
 
-    if (this.params.behaviour.highlightingLibrary === 'prismjs') {
 
-      return Prism.highlight(code, Prism.languages[language], language);
+    // TODO: Fix prismjs highlighting for other languages
+    if (this.params.highLightingBehaviour.highlightingLibrary === 'prismjs') {
+
+      return prism.highlight(code, prism.languages[language], language);
 
     } else {
 
